cdd91a9e9336e472ebb3c7efa8c3dcf773577109
antoniozandate <antonio.zandate@metricamovil.com>
2021-06-15 12:09:34 -0500

    Setup project

---
+++ app/src/main/java/mx/edu/itl/equipo3/asistenciasapp/CargarAsistenciasActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.itl.equipo3.asistenciasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CargarAsistenciasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cargar_asistencias);
+    }
+}
